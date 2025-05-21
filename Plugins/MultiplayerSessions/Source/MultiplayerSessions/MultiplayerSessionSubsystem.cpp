422390fd87bde075aa4e944251f138dbcb824694
Weiwei <93496699+wei858910@users.noreply.github.com>
2025-05-21 18:38:15 +0800

    创建MultiplayerSessionSubsystem
    MultiplayerSessionSubsystem 在游戏生命全周期中 对 OnlineSubsystem 进行管理

---
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/MultiplayerSessionSubsystem.cpp
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MultiplayerSessionSubsystem.h"
+
+#include "OnlineSubsystem.h"
+
+UMultiplayerSessionSubsystem::UMultiplayerSessionSubsystem()
+{
+	// 获取默认的在线子系统实例指针。在线子系统负责管理游戏的网络相关功能，
+	// 不同的平台（如 Steam、Epic Games 等）有不同的实现。
+	IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
+	if (Subsystem)
+	{
+		// 若成功获取在线子系统实例，则从该子系统中获取会话接口指针。
+		// 会话接口提供了创建、查找、加入和销毁游戏会话等功能。
+		SessionInterface = Subsystem->GetSessionInterface();
+	}
+}
---
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/MultiplayerSessionSubsystem.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Interfaces/OnlineSessionInterface.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "MultiplayerSessionSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERSESSIONS_API UMultiplayerSessionSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	UMultiplayerSessionSubsystem();
+
+protected:
+
+private:
+	IOnlineSessionPtr SessionInterface;
+};
